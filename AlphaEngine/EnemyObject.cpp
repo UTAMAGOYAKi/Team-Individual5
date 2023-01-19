5f85de5e8db07187d39384c702367038e8830e16
JJY450Z <junjiey450@gmail.com>
2023-01-19 15:23:29 +0800

    Added Meshes and Texture tutorial

---
+++ AlphaEngine/EnemyObject.cpp
@@ -0,0 +1,7 @@
+//struct Enemy {
+//
+//	int HP{};
+//	int ATK{};
+//	Element Enemy_Type; //Enum from element list
+//
+//};
--- AlphaEngine/Main.cpp
+++ AlphaEngine/Main.cpp
@@ -7,7 +7,7 @@
 
 // ---------------------------------------------------------------------------
 // main
-//comment
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
@@ -30,6 +30,29 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// reset the system modules
 	AESysReset();
 
+	//_______________MESH CREATION________________________________________
+	// Pointer to Mesh
+	AEGfxVertexList* pMesh = 0;
+	// Informing the library that we're about to start adding triangles
+	AEGfxMeshStart();
+	// This shape has 2 triangles that makes up a square
+	// Color parameters represent colours as ARGB in hexadecimal
+	// (e.g. 0xFFFF0000 is red, 0xFF00FF00 is green)
+	// UV coordinates to read from loaded textures
+	AEGfxTriAdd(
+		-0.5f, -0.5f, 0xFFFFFFFF, 0.0f, 0.0f,
+		0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
+	AEGfxTriAdd(
+		0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
+		0.5f, 0.5f, 0xFFFFFFFF, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
+	// Saving the mesh (list of triangles) in pMesh
+	pMesh = AEGfxMeshEnd();
+
+	//ADDING TEXTURE
+	AEGfxTexture* pTex = AEGfxTextureLoad("Assets/PlanetTexture.png");
+
 
 
 	// Game Loop
@@ -38,6 +61,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		// Informing the system about the loop's start
 		AESysFrameStart();
 
+
+
 		// Handling Input
 		AEInputUpdate();
 
@@ -45,7 +70,38 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 		// Your own rendering logic goes here
-
+		// Set the background to black.
+		AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
+		// Tell the engine to get ready to draw something with texture.
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set the tint to white, so that the sprite can 
+		// display the full range of colors (default is black).
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Set blend mode to AE_GFX_BM_BLEND
+		// This will allow transparency.
+		AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		AEGfxSetTransparency(1.0f);
+		// Set the texture to pTex
+		AEGfxTextureSet(pTex, 0, 0);
+		// Create a scale matrix that scales by 100 x and y
+		AEMtx33 scale = { 0 };
+		AEMtx33Scale(&scale, 100.f, 100.f);
+		// Create a rotation matrix that rotates by 45 degrees
+		AEMtx33 rotate = { 0 };
+		AEMtx33Rot(&rotate, PI / 4);
+		// Create a translation matrix that translates by
+		// 100 in the x-axis and 100 in the y-axis
+		AEMtx33 translate = { 0 };
+		AEMtx33Trans(&translate, 100.f, 100.f);
+		// Concatenate the matrices (TRS)
+		AEMtx33 transform = { 0 };
+		AEMtx33Concat(&transform, &rotate, &scale);
+		AEMtx33Concat(&transform, &translate, &transform);
+		// Choose the transform to apply onto the vertices 
+		// of the mesh that we are choose to draw in the next line.
+		AEGfxSetTransform(transform.m);
+		// With the above settings, draw the mesh.
+		AEGfxMeshDraw(pMesh, AE_GFX_MDM_TRIANGLES);
 
 		// Informing the system about the loop's end
 		AESysFrameEnd();
@@ -55,6 +111,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 			gGameRunning = 0;
 	}
 
+	AEGfxTextureUnload(pTex);
+	AEGfxMeshFree(pMesh);
 
 	// free the system
 	AESysExit();
