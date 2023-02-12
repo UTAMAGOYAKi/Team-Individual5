968b7ee0e4473fa094f1facaa50251cd1d3b1c21
zonedrake <t33daniel@gmail.com>
2023-02-12 22:48:02 +0800

    Some more UI

--- AlphaEngine/UI.cpp
+++ AlphaEngine/UI.cpp
@@ -2,11 +2,15 @@
 
 
 
-/*---------------------*/
+/*---------------------------------*/
 //	Defines
-/*---------------------*/
+//  All values tenative to changes
+/*---------------------------------*/
 
-const float hp_bar_height = 2.0f;
+const float hp_bar_height	= 2.0f;
+const float hp_bar_offset	= 5.0f;
+const float name_offset		= 2.0f;
+
 const float hp_bar_player_width = 10.0f;
 const float hp_bar_enemy_width = 10.0f;
 const float hp_bar_boss_width = 20.0f;
@@ -15,9 +19,42 @@ extern s8 font;	//to be changed in the future? Dependent on how it font will be 
 
 void enemy_info(enemy ref)
 {
-	s8* text;
+	std::string tmp{ ref.name };
+
+	AEGfxPrint(font, (s8*)tmp.c_str(), ref.pos_x, ref.pos_y+name_offset, 0.0f, 0.0f, 0.0f, 0.0f);
+	tmp.clear();
+	tmp = std::to_string(ref.hp);
+	//The rest requires referencing value to do a hp bar size
+	//Request for enemy struct to carry Max_hp of enemy at creation as well
 
 
-	AEGfxPrint(font, text	,ref.pos_x,ref.pos_y	,0.0f	,0.0f,0.0f,0.0f);
+}
+
+void player_hp_bar(int hp, aabb play_pos)
+{
+	//format for player?
+}
+
+
+void name_bar(std::string name, coord place)
+{
+	AEGfxPrint(font, (s8*)name.c_str(), place.x, place.y + name_offset, 0.0f, 0.0f, 0.0f, 0.0f);
+	//remeber to check for centering offset %
+}
+
+void name_bar(std::string name, aabb place)
+{
+	AEGfxPrint(font, (s8*)name.c_str(), place.mid.x, place.mid.y, 0.0f, 0.0f, 0.0f, 0.0f);
+	//remeber to check for centering offset %
+}
+
+
+
+void pause_menu_draw(AEGfxTexture* menu, AEGfxTexture* buttons)
+{
+	//AEGfxGetWindowHeight(); doess not work, remember to bring up for the documentation
+	float tmpx = AEGetWindowWidth(); // to ensure the pause menu is centred 
+	float tmpy = AEGetWindowHeight();
+
 
 }
--- AlphaEngine/UI.h
+++ AlphaEngine/UI.h
@@ -7,28 +7,28 @@
 #include "Buttons.h"
 #include "AEEngine.h"
 #include "EnemyObject.h"
+#include <string>
 
-struct button
-{
-	dragdrop* box;
-	int ref;
-	AEGfxTexture* mesh;
-};
+
 
  
 
 
 void enemy_info(enemy ref);
 
-void player_hp_bar(int hp);
+void player_hp_bar(int hp, aabb play_pos);
 
-void name_bar(int hp, coord place);
+void name_bar(std::string name, coord place);
+void name_bar(std::string name, aabb place);
 
-void name_bar(int hp, aabb place);
+//void name_bar(int hp, aabb place);
 
 //void status_effect_ui(enemy ?);
 
-void button_draw();
+// How do we plan to assign textures to buttons
+//void button_draw();
+
+void pause_menu_draw(AEGfxTexture* menu, AEGfxTexture* buttons);
 
 
 
@@ -36,9 +36,7 @@ void button_draw();
 
 
 
-
-
-#endif // !1
+#endif //UI_h
 
 
 
