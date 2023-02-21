37205f01dbeebe6d60c3cad3a71fb91b7a063921
zonedrake <t33daniel@gmail.com>
2023-02-21 15:29:54 +0800

    Buttons maps and request

---
+++ AlphaEngine/Map.h
@@ -0,0 +1,24 @@
+#ifndef map_h
+#define map_h
+
+
+// to be drawn after victory, but before unloading and freeing?
+//fake it till you make it
+
+//entering stage "animation"
+void transistion_enter();
+
+// To be drawn till after loading and init?
+// exiting stage "animation"
+void transistion_exit();
+
+//entering boss stage
+void transistion_boss();
+
+//map?
+
+
+
+
+#endif // !map_h
+
